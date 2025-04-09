cb3e5d2e132297501f328c8de20706a60a4f066b
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-09 22:03:33 +0600

    Character variable printing problem

---
+++ Week1/Module3.5/variables.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int a; 
+    scanf("%d",&a);
+    printf("%d\n",a);
+
+    long long int b;
+    scanf("%lld",&b);
+    printf("%lld\n",b);
+
+    float c; 
+    scanf("%f",&c);
+    printf("%.2f\n",c);
+
+    scanf(" ");
+    char d;
+    scanf("%c",&d);
+    printf("%c\n",d);
+
+    return 0;
+}
