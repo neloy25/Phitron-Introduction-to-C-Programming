7aeddb8b40e834e7ef1a7b4f5b40c81e5e1996fe
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-13 23:46:53 +0600

    Module 3: fibonacci series

--- Week1/Module3/fibonacci_series.c
+++ Week1/Module3/fibonacci_series.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 
 int main(){
-    int count = 0;
-        int n;
-        scanf("%d",&n);
-        int st = 0;
-        printf("%d ",st);
-        int end = 1;
-        printf("+ %d ",end);
-        int sum = st + end;
+    int n;
+    scanf("%d",&n);
+    int st = 0;
+    printf("%d ",st);
+    int end = 1;
+    printf("+ %d ",end);
+    int sum = st + end;
 
-        while(sum <= n){
-            printf("+ %d ",sum);
-            st = end;
-            end = sum;
-            sum = st + end;
-        }
-        return 0;
+    while(sum <= n){
+        printf("+ %d ",sum);
+        st = end;
+        end = sum;
+        sum = st + end;
+    }
+    return 0;
 }
