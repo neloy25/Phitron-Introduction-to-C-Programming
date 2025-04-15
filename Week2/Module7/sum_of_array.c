6ce1d6605138db4e27cde7a5cc2ac639bb690eaf
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-15 23:43:56 +0600

    Array: Sum of all elements

--- Week2/Module7/sum_of_array.c
+++ Week2/Module7/sum_of_array.c
@@ -7,7 +7,10 @@ int main() {
     int sum = 0;
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-        sum+= arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+        sum+=arr[i];
     }
     printf("%d", sum);
     return 0;
