3d673dc85942005d07d7f9014f9223bf22f47d76
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-28 12:24:43 +0600

    Mid term break : Zeros and Ones 2

---
+++ Week3/ExtraContest/zerosAndOnes2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int x;
+    scanf("%d", &x);
+    if(arr[x-1] == 0) arr[x-1] = 1;
+    else arr[x-1] = 0;
+    for (int i = 0; i < n; i++) {
+      printf("%d ", arr[i]);
+    }
+    return 0;
+}
