8f3d9a8a5afb30327c9c17d21282cb561b0b5e09
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-04 12:24:45 +0600

    Module 14: Diamond Pattern

---
+++ Week4/Module13/diamondPattern.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int star = 1, space = n - 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= space; j++) {
+            printf(" ");
+        }
+        for (int j = 1;j <= star; j++) {
+            printf("*");
+        }
+        printf("\n");
+        star+=2;
+        space--;
+    }
+
+    star = 2 * n - 3,space = 1;
+    for (int i = 1; i <= n; i++) {
+
+        for (int j = 1; j <= space; j++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= star; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+        star-=2;
+        space++;
+    }
+    return 0;
+}
