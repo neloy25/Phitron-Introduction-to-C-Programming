51f4fe9764a6af144cdb90b9186cec5798f7514e
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-04 16:41:30 +0600

    Sum of Two Equal X

---
+++ Week4/Module13/sumOfTwoEqualX.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int x;
+    scanf("%d", &x);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == x){
+                printf("%d %d", i + 1,j + 1);
+                return 0;
+            }
+        }
+    }
+    printf("Not found");
+    return 0;
+}
