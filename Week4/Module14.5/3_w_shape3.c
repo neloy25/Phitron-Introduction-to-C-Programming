18f5643ea4a08ed776d4f7a42435b6f89a3cadb6
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-06 18:24:51 +0600

    3_w_shape3.c

---
+++ Week4/Module14.5/3_w_shape3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i = 1; i <= n; i++){
+        for(int spc = n - i; spc > 0; spc--){
+            printf(" ");
+        }
+        printf("*");
+        for(int j = 1; j < i;j++){
+            printf("**");
+        }
+        printf("\n");
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int spc = 1; spc < i; spc++){
+            printf(" ");
+        }
+        printf("*");
+        for(int j = n - i; j >= 1;j--){
+            printf("**");
+        }
+        printf("\n");
+    }
+    return 0;
+}
