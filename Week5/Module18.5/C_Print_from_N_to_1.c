5326e164cacb6ea67ae8e658e408a9ab210a5270
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-15 12:44:16 +0600

    Module 18: Print n t0 1

---
+++ Week5/Module18.5/C_Print_from_N_to_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ 
+void print_1_to_n(int i,int n){
+    if(i == n + 1) return;
+    print_1_to_n(i+1,n);
+    printf("%d\n",i);
+}
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+    print_1_to_n(1,n);
+    return 0;
+}
